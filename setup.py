557b59e3db9d36a3950fd250146b29fd3539a095
Doug Wiegley <doug@parksidesoftware.com>
2016-05-25 23:58:11 -0600

    fix manifest, bump 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "a10-neutronclient",
-    version = "0.1.2a",
+    version = "0.1.2",
     packages = find_packages(),
 
     author = "A10 Networks",
